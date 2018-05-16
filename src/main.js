2c0d7e83dfcac9f436a23e51c271d66b658c18bd
psycadelik <francisadrian9@outlook.com>
2018-05-16 07:43:04 -0400

    components added

---
+++ src/main.js
@@ -0,0 +1,64 @@
+import Vue from 'vue';
+import './style.scss';
+
+import genres from './util/genres';
+
+new Vue({
+    el:'#app',
+    components: {
+        'movie-list':{
+            template:`<div id="movie-list">
+                            <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                    </div>`,
+            data(){
+                return {
+                    movies:[
+                        { title: 'Pulp fiction' },
+                        { title: 'The Black Panther' },
+                        { title: 'Infinity war' }
+                    ]
+                };
+
+            }
+        },
+        'movie-filter':{
+            data(){
+                return {
+                    genres
+                }
+            },
+            template:`<div id="movie-filter">
+                            <h2>Filter results</h2>
+                            <div class="filter-group">
+                                <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+                            </div>
+                            
+                    </div>`,
+            methods:{
+              checkFilter(){
+                  console.log('checkFilter');
+              }
+            },
+            components:{
+                'check-filter':{
+                    data(){
+                        return {
+                            checked:false
+                        }
+                    },
+                    props: ['title'],
+                    template: `<div v-bind:class="{ 'check-filter':true ,active: checked }" v-on:click="checkFilter">
+                                <span class="checkbox"></span>
+                                <span class="check-filter-title">{{title}}</span>
+                                </div>`,
+                    methods:{
+                        checkFilter(){
+                            this.checked = !this.checked
+                            this.$emit('check-filter');
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
